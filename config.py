a03bdf6e1c3cf7575cc813753d3d0b82faa0e4ce
federibet <fribetto@gmail.com>
2024-08-06 16:19:12 -0300

    Minor update

--- config.py
+++ config.py
@@ -3,6 +3,6 @@
 import os
 
 # Load configuration from environment variables or provide default values
-COHERE_API_KEY = os.getenv('COHERE_API_KEY', 'pfK329FllWHqYXfGP0mwHi79OQEU1ehVMJV6PvPH')
+COHERE_API_KEY = os.getenv('COHERE_API_KEY', 'insert_your_key')
 DOCUMENT_FILE_PATH = os.getenv('DOCUMENT_FILE_PATH', 'documento.docx')
 SUPPORTED_LANGUAGES = ['es', 'en', 'pt']
